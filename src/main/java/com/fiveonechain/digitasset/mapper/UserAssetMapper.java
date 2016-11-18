91bbccace4766876a2fd234772ed0dced5b0f42d
Shichao Yuan <oahcihsnauy@gmail.com>
2016-11-18 21:20:30 +0800

    update user_asset table

--- src/main/java/com/fiveonechain/digitasset/mapper/UserAssetMapper.java
+++ src/main/java/com/fiveonechain/digitasset/mapper/UserAssetMapper.java
@@ -13,7 +13,10 @@ CREATE TABLE `user_asset` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `asset_id` int(11) NOT NULL,
   `user_id` int(11) NOT NULL,
+  `contract_id` int(11) NOT NULL,
   `balance` int(11) NOT NULL,
+  `trade_balance` int(11) NOT NULL,
+  `lock_balance` int(11) NOT NULL,
   `create_time` timestamp NOT NULL default CURRENT_TIMESTAMP,
   `update_time` timestamp NOT NULL default CURRENT_TIMESTAMP on update CURRENT_TIMESTAMP,
   `status` tinyint(4) NOT NULL default '0',
@@ -21,6 +24,6 @@ CREATE TABLE `user_asset` (
   PRIMARY KEY (`id`),
   UNIQUE KEY `idx_assetid_userid` (`asset_id`, `user_id`)
 
-) DEFAULT CHARSET=utf8 COLLATE = utf8_bin
+)
 
 */
