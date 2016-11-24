204c6aca7568d73318c38dfd11ac5a06df41b7ef
Shichao Yuan <oahcihsnauy@gmail.com>
2016-11-24 10:54:23 +0800

    update

--- src/main/java/com/fiveonechain/digitasset/mapper/UserAssetRecordMapper.java
+++ src/main/java/com/fiveonechain/digitasset/mapper/UserAssetRecordMapper.java
@@ -1,9 +1,17 @@
 package com.fiveonechain.digitasset.mapper;
 
+import com.fiveonechain.digitasset.domain.UserAssetRecord;
+import org.apache.ibatis.annotations.Insert;
+
 /**
  * Created by yuanshichao on 2016/11/14.
  */
 public interface UserAssetRecordMapper {
+
+
+    @Insert("INSERT INTO user_asset_record ")
+    int insert(UserAssetRecord record);
+
 }
 
 
@@ -13,16 +21,15 @@ CREATE TABLE `user_asset_record` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `asset_id` int(11) NOT NULL,
   `from_user_id` int(11) NOT NULL,
-  `operation` tinyint(4) NOT NULL,
-  `old_balance` int(11) NOT NULL,
-  `new_balance` int(11) NOT NULL,
   `to_user_id` int(11) NOT NULL,
+  `operation` tinyint(4) NOT NULL,
+  `amount` int(11) NOT NULL,
   `contract_id` int(11) NOT NULL,
   `create_time` timestamp NOT NULL default CURRENT_TIMESTAMP,
 
   PRIMARY KEY (`id`),
-  UNIQUE KEY `idx_assetid_userid` (`asset_id`, `from_user_id`,`to_user_id`)
+  UNIQUE KEY `idx_assetid_userid` (`asset_id`, `from_user_id`)
 
-) DEFAULT CHARSET=utf8 COLLATE = utf8_bin
+)
 
 */
