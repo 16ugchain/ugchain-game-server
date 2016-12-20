9b7889b0e860c9be6eb1d010f1e24d8977699779
Shichao Yuan <oahcihsnauy@gmail.com>
2016-12-20 14:32:08 +0800

    update

--- src/main/java/com/fiveonechain/digitasset/ChcDigitassetApplication.java
+++ src/main/java/com/fiveonechain/digitasset/ChcDigitassetApplication.java
@@ -3,9 +3,11 @@ package com.fiveonechain.digitasset;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableAsync
+@EnableScheduling
 public class ChcDigitassetApplication {
 
 	public static void main(String[] args) {
